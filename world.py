2dfd677fdebed0d3f6f01fd45e4abb1f0bb6512d
beandrea <arkflame99@gmail.com>
2025-06-23 11:40:04 -0400

    room gen

--- world.py
+++ world.py
@@ -1,4 +1,5 @@
 from random import randrange
+from random import randint
 
 class World:
     def __init__(self):
@@ -23,5 +24,28 @@ class World:
             temp = self.grid
             for j in range(self.__width):
                 for k in range(self.__height):
-                    nWallCnt = 0
-                    pass
+                    nWallCnt = self.__wallCount(temp, j, k)
+
+                    r = randint(2, 4)
+                    if nWallCnt > r:
+                        self.grid[j][k] = "wall"
+                    else:
+                        self.grid[j][k] = "floor"
+
+    def __wallCount(self, temp, inX, inY):
+        cnt = 0
+        for i in range(inX - 1, inX + 1):
+            for j in range(inY - 1, inY + 1):
+                if ((i > 0 and j > 0 ) and
+                    (i < self.__width and j < self.__height)):
+
+                    if i != inX and j != inY:
+                        if temp[i][j] == "wall":
+                            cnt += 1
+                else:
+                    cnt += 1
+        return cnt
+
+    def genRoom(self):
+        self.__genNoise()
+        self.__smoothing()
