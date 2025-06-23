844971af29d596838dc859b3b0fb5337c6c451e1
beandrea <arkflame99@gmail.com>
2025-06-23 12:53:39 -0400

    basic room gen

--- world.py
+++ world.py
@@ -1,36 +1,37 @@
 from random import randrange
 from random import randint
+import pygame
 
 class World:
     def __init__(self):
         self.__height = 14
         self.__width = 24
         self.monList = []
-        self.grid = []
+        self.__grid = []
 
     def __genNoise(self):
         noiseDen = 55
         for i in range(self.__width):
-            self.grid[i] = []
+            self.__grid[i] = []
             for j in range(self.__height):
                 gen = randrange(0, 100)
                 if gen > noiseDen:
-                    self.grid[i][j] = "floor"
+                    self.__grid[i][j] = "floor"
                 else:
-                    self.grid[i][j] = "wall"
+                    self.__grid[i][j] = "wall"
 
     def __smoothing(self):
         for i in range(75):
-            temp = self.grid
+            temp = self.__grid
             for j in range(self.__width):
                 for k in range(self.__height):
                     nWallCnt = self.__wallCount(temp, j, k)
 
                     r = randint(2, 4)
                     if nWallCnt > r:
-                        self.grid[j][k] = "wall"
+                        self.__grid[j][k] = "wall"
                     else:
-                        self.grid[j][k] = "floor"
+                        self.__grid[j][k] = "floor"
 
     def __wallCount(self, temp, inX, inY):
         cnt = 0
@@ -49,3 +50,11 @@ class World:
     def genRoom(self):
         self.__genNoise()
         self.__smoothing()
+
+    def show(self, surf):
+        for i in range(self.__width):
+            for j in range(self.__height):
+                if self.__grid[i][j] == "wall":
+                    pygame.draw.rect(surf, "purple", (i * 50, j * 50, 50, 50))
+                else:
+                    pygame.draw.rect(surf, "gold", (i * 50, j * 50, 50, 50))
