717bc2e7e592ba3511afef0e63a8a89adc063588
beandrea <arkflame99@gmail.com>
2025-08-04 05:12:30 -0400

    Limitted health fount uses

--- world/healFount.py
+++ world/healFount.py
@@ -1,7 +1,10 @@
 class Fountain:
-    def __init__(self, x, y):
+    def __init__(self, x, y, uses):
         self.__x = x
         self.__y = y
+        self.__uses = uses
 
     def heal(self, player):
-        player.modHealth()
+        if self.__uses > 0:
+            player.modHealth()
+            self.__uses -= 1
